bfb253f768bded16aa12771c7df5c9623119483a
benjamin.crouch.ii <benjamin.crouch.ii@bwcii.com>
2024-01-11 18:04:39 +0000

    Issues returning values from getCatImgURL to app

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require('express');
+const http = require('node:http');
+const https = require('node:https')
 const app = express();
 // Need to pull this from google secret manager
 const cat_api_key = process.env.CAT_API_KEY
 
-app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
+app.get('/randomCats', (req, res) => {
+  console.log(`Cat API Key : ${cat_api_key}`);
+  res.send("test\n");
 });
 
 const port = parseInt(process.env.PORT) || 8081;
-console.log({port});
+
 app.listen(port, () => {
-  console.log(`helloworld: listening on port ${port}`);
-  console.log(`$CAT_API_KEY : ${cat_api_key}`);
+  console.log(`listening on port ${port}`);
 });
+
+function getCatImgURL () {
+  https.get('https://api.thecatapi.com/v1/images/search', (res) => {
+  console.log('statusCode:', res.statusCode);
+  console.log('headers:', res.headers);
+
+  res.on('data', (d) => {
+    process.stdout.write(d);
+  });
+
+}).on('error', (e) => {
+  console.error(e);
+});
+}
