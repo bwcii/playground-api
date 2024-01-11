2e8aaa0c6f2f94fb98f6c12bd9e6ba800aa4eeea
benjamin.crouch.ii <benjamin.crouch.ii@bwcii.com>
2024-01-11 21:04:25 +0000

    So Close

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
 const express = require('express');
 const http = require('node:http');
-const https = require('node:https')
+const https = require('node:https');
+const { url } = require('node:inspector');
 const app = express();
 // Need to pull this from google secret manager
 const cat_api_key = process.env.CAT_API_KEY
 
 app.get('/randomCats', (req, res) => {
-  console.log(`Cat API Key : ${cat_api_key}`);
-  res.send("test\n");
+  //console.log(`Cat API Key : ${cat_api_key}`);
+  var catURL = getCatImgURL();
+  console.log(catURL);
+  res.send(catURL);
 });
 
 const port = parseInt(process.env.PORT) || 8081;
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+  console.log(`listening on port ${port}\n`);
 });
 
 function getCatImgURL () {
-  https.get('https://api.thecatapi.com/v1/images/search', (res) => {
-  console.log('statusCode:', res.statusCode);
-  console.log('headers:', res.headers);
-
+  https.get('https://api.thecatapi.com/v1/images/search', (res, next) => {
+  res.setEncoding('utf8');
+  //console.log('statusCode:', res.statusCode);
+  //console.log('headers:', res.headers);
+  var output = "";
+  var myurl = "";
+  
   res.on('data', (d) => {
-    process.stdout.write(d);
+    output += d;
+  });
+  res.on('end', (e) => {
+    myurl = JSON.parse(output)[0].url;
+    //process.stdout.write(url);
+    console.log(myurl);
+    return myurl;
   });
 
 }).on('error', (e) => {
